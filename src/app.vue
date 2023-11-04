be00215d2f34d3a08f5b63ae48f5379b523b149c
AWESOME247 <omoye6996@gmail.com>
2023-11-04 22:55:20 +0100

    done

--- src/app.vue
+++ src/app.vue
@@ -1,7 +1,7 @@
 <template>
   <VitePwaManifest />
   <NuxtLayout>
-    <NuxtLoadingIndicator color="#228b22" />
+    <NuxtLoadingIndicator color="#228b22" :height="5" />
     <NuxtPage />
   </NuxtLayout>
 </template>
